82c8e20c08c86b4a56848a50258823c0d3e1efe2
Nishant Kedare <kedarenishant11111@gmail.com>
2024-07-27 11:48:52 +0530

    Csv Code Integrated

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -1,6 +1,42 @@
 <?php
-use Illuminate\Support\Facades\Http;
+
+use App\Models\Data;
+use PhpOffice\PhpSpreadsheet\IOFactory;
 use function Livewire\Volt\{state, mount};
+
+
+$fetchDataFromCsv = function () {
+
+    $inputFileName = public_path('images/data.csv');
+
+    // Load CSV file
+    $spreadsheet = IOFactory::load($inputFileName);
+
+    // Get the active sheet (first sheet in the CSV)
+    $sheet = $spreadsheet->getActiveSheet();
+
+    // Specify the cell you want to read
+    $cellCoordinate = 'A1'; // For example, cell A1
+
+    // Get the value of the specified cell
+    $cellValue = $sheet->getCell($cellCoordinate)->getValue();
+
+    $data = $cellValue;
+
+    $highestColumn = $sheet->getHighestColumn();
+    $highestRow = $sheet->getHighestRow();
+
+    $data = [];
+    for ($i = 2; $i <= $highestRow; $i++) {
+        $array = [];
+        for ($a = 'A'; $a <= $highestColumn; $a++) {
+            $array[$sheet->getCell($a . '1')->getValue()] = $sheet->getCell($a . $i)->getValue();
+        }
+        $data[] = $array;
+    }
+
+    Data::insert($data);
+};
 ?>
 
 <div class="grid grid-cols-1 gap-6">
